4040508ea4b2353baf3379722dc174e24ed8f03f
nakalua <luanadez13@gmail.com>
2020-02-24 18:57:08 -0300

    googole maps key

---
+++ src/components/auth/Signup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="sigunp container">
+    <h2>Signup</h2>
+    <!-- <form class="card-panel">
+      <h2 class="center deep-purple-text">Signup</h2>
+      <div class="field">
+        <label for="email">Email:</label>
+        <input type="email" name="email">
+      </div>
+      <div class="field">
+        <label for="password">Password:</label>
+        <input type="password" name="password">
+      </div>
+      <div class="field">
+        <label for="alias">Alias:</label>
+        <input type="text" name="alias">
+      </div>
+      <div class="field center">
+        <button class="btn deep-purple">
+          Signup
+        </button>
+      </div>
+    </form> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
