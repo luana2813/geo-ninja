fdb74ca9442d4082b97e5c81634c98a00931f4bf
nakalua <luanadez13@gmail.com>
2020-02-24 12:01:26 -0300

    first commit

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBWfpGgs-MP07UWUKmwPe1kN8qxKg8RIUM",
+    authDomain: "geo-ninja-ceda7.firebaseapp.com",
+    databaseURL: "https://geo-ninja-ceda7.firebaseio.com",
+    projectId: "geo-ninja-ceda7",
+    storageBucket: "geo-ninja-ceda7.appspot.com",
+    messagingSenderId: "854335846023",
+    appId: "1:854335846023:web:d48e5a143baa2c58dbea22",
+    measurementId: "G-BTX1C6GKYQ"
+  };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebaseApp.firestore()
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import GMap from '@/components/home/GMap'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'GMap',
+      component: GMap
+    }
+  ]
+})
+
+
